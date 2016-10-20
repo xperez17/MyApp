f977309bb12f90045db91cdd0b5a9ef84afbc20e
xperez <xperez@gips.org>
2016-10-20 14:45:58 -0500

    took out unnecessary things

--- app/src/main/java/com/example/xperez/myapp/LogIn.java
+++ app/src/main/java/com/example/xperez/myapp/LogIn.java
@@ -1,16 +1,14 @@
 package com.example.xperez.myapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class LogIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.LogIn_SignUp);
-        setContentView(R.layout.login_sign_up);
+        setContentView(R.layout.sign_up);
     }
 }
-
-
