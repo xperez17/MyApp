ebe18158ddfb4293d1f8d8acb7eed5e038afbacf
xperez <xperez@gips.org>
2016-11-03 14:16:28 -0500

    took out unnecessary things

--- app/src/main/java/com/example/xperez/myapp/MainActivity.java
+++ app/src/main/java/com/example/xperez/myapp/MainActivity.java
@@ -6,71 +6,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-
 public class MainActivity extends AppCompatActivity {
 
-    public Button log1;
-    public Button create;
+    @Override
+        protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_sign_up);
 
-    public void one(){
-        log1 = (Button)findViewById(R.id.log1);
-        log1.setOnClickListener(new View.OnClickListener(){
+        Button log1 = (Button) findViewById(R.id.log1);
+        log1.setOnClickListener (new View.OnClickListener() {
+            @Override
+             public void onClick(View v) {
+
+                Intent x = new Intent(MainActivity.this, LogInPage.class);
+                startActivity(x);
+            }
+        });
+
+        //protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_sign_up);
+
+        Button create = (Button) findViewById(R.id.create);
+        create.setOnClickListener (new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                Intent y = new Intent(MainActivity.this, SignUp.class);
+                startActivity(y);
+             }
 
-                Intent x = new Intent(MainActivity.this, LogInPage.class);
-
-                startActivity(x);
-
-            }
         });
-    }
-
-
-
-             @Override
-             protected void onCreate(Bundle savedInstanceState) {
-             super.onCreate(savedInstanceState);
-             setContentView(R.layout.login_sign_up);
-             one();
-
-
-
-                            }
-
-
-
-public class MainActivity extends AppCompatActivity {
-    private class onClickLitsenter implements View.OnClickListener {
-
-    public void two() {
-        create = (Button) findViewById(R.id.create);
-        create.setOnClickListener(new view.onClickLitsenter(){
-        @Override
-        public void onClick(View v) {
-
-
-
-            Intent y = new Intent(MainActivity.this, SignUp.class);
-            startActivity(y);
-        }
-        });
-    }
-
-
-
-
-
-
-        @Override
-        protected void onCreate2 (Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-
-            setContentView(R.layout.login_sign_up);
-            two();
-
-
-        }
     }
 }
